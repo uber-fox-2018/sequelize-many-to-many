1969701cbbb3bd7c2f2539eeca0e9c107ef8354d
wisnugautama <ngurahwisnugautama@gmail.com>
2018-07-31 17:39:28 +0700

    add helper for score

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.locals.getScore = require('./helper/getScore')
 
 app.use('/', Home)
 app.use('/students', Student)
--- helpers/getScore.js
+++
@@ -1,8 +0,0 @@
-function getScore(student) {
-    if (student.score == null) {
-        return `link`
-    }
-    return student.score
-}
-
-module.exports = getScore
