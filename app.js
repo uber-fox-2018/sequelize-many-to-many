153620f82b9e42d0ea8168377bd0cc64ae8f1c94
joanlamrack <joanlamrack@gmail.com>
2018-08-01 09:01:40 +0700

    final lagi

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const app = require("express")();
 const express = require("express");
 const routes = require("./routes");
 const body = require("body-parser");
+const GradeHelper = require("./helpers/mainhelper.js");
 
+app.use((req,res,next)=>{
+	res.locals.helper = GradeHelper;
+	next();
+});
 
 app.use(body.urlencoded({extended:true}));
 app.use(body.json());
